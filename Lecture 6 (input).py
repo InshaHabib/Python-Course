b38f3a48f28174908fa9358a73f625fa8d20db51
Insha Habib <145709260+InshaHabib@users.noreply.github.com>
2024-07-15 23:46:05 +0500

    python file

---
+++ Lecture 6 (input).py
@@ -0,0 +1,18 @@
+# Taking user input
+
+a = input("Enter your name:")
+print("My name is", a)
+
+x = (input("Enter first number:"))
+# x = int(input("Enter first number:"))
+y = (input("Enter second number:"))
+# y = int(input("Enter second number:"))
+
+# print(int(x) + int(y))
+# print(int(x) - int(y))
+# print(x - y) #ERROR OCCUR
+# print(x + y)
+# print(x * y) #ERROR OCCUR
+# print(x / y) #ERROR OCCUR
+# print(int(x) * int(y))
+print(int(x) / int(y)) # Also give answer in point
