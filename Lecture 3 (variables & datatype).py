ab5b1dde1e48d132e8845fbdf736d1e7d98664d8
Insha Habib <145709260+InshaHabib@users.noreply.github.com>
2024-07-15 23:41:03 +0500

    python file

---
+++ Lecture 3 (variables & datatype).py
@@ -0,0 +1,28 @@
+# Variables is a container that holds data
+a=2.1
+print(a)
+b="Insha"
+print(b)
+c= True
+d=complex(8,2) # i value
+print(c)
+a1=5
+print(a+a1)
+
+# Datatypes specifies the type of value a variable holds
+# Python mn sb kuch object hota haii
+# int, float, complex, str, bool, list(different datatypes), tuple(not changed), dict(mapped data)
+
+print("The type of a is", type(a))
+print("The type of c is", type(c))
+print("The type of b is", type(b))
+print("The type of d is", type(d))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
