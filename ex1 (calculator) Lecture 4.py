116add30484c5be107b1b8f47018c77d5fda8fcb
Insha Habib <145709260+InshaHabib@users.noreply.github.com>
2024-07-17 22:52:08 +0500

    exercise 1 commit

---
+++ ex1 (calculator) Lecture 4.py
@@ -0,0 +1,42 @@
+# Operators
+""" print(5+6)
+print(15-6)
+print(5*6)
+print(5/6)
+print(5//6) #Floor Division (without point)
+print(5%6) #Remainder
+print(5**6) #Power of 5 """
+print(5%6) #Remainder
+
+print("********Calculator1********")
+#a=(input("enter a value ="))
+#b=(input("enter b value ="))
+a=int(input("Enter first Value : "))
+b=int(input("Enter second Value: "))
+
+print("Addition of", a ,"+", b ,"are",a+b)
+print("Subtraction of", a ,"-", b, "are", a-b)
+print("Multiplication of", a, "*" ,b, "are", a*b)
+print("Division of", a ,"/", b, "are", a/b)
+print("Floor Division of", a ,"//", b, "are", a//b)
+print("Remainder of", a ,"%", b, "are", a%b)
+
+
+# print("********Calculator2********")
+# def add_numbers(num1, num2):
+#     try:
+#         result = num1 + num2
+#         return result
+#     except Exception as e:
+#         return f"Error: {e}"
+
+""" Example usage:
+You can use this function by passing two numbers as arguments.
+You can use integers, floats, or any numeric data type. """
+
+# num1 = 5
+# num2 = 3.2
+
+# result = add_numbers(num1, num2)
+
+# print(f"The sum of {num1} and {num2} is: {result}")
