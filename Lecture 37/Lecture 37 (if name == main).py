fe001a54f683c5c97c6cb1416632dc4258014152
Insha Habib <145709260+InshaHabib@users.noreply.github.com>
2024-08-29 15:46:30 +0500

    python file

--- Lecture 37/Lecture 37 (if name == main).py
+++
@@ -1,3 +0,0 @@
-import inshafile
-
-inshafile.welcome()
--- Lecture 37/inshafile.py
+++
@@ -1,7 +0,0 @@
-# for lecture 37
-def welcome():
-    print("Hey you are welcome from insha")
-    
-# print(__name__)
-if __name__ == "__main__":
-    welcome()
