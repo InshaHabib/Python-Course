477a28b9961fc24ebb9be739e4c3307e40f4ae24
Insha-Habib <145709260+InshaHabib@users.noreply.github.com>
2024-09-28 21:38:48 +0500

    python file

---
+++ Lecture 46 (Intro to OOPs).py
@@ -0,0 +1,27 @@
+# Introduction to OOPs
+# 1. Procedural Programming
+def hello():
+    print("Hello World")
+hello()
+
+sales1 = 6000
+profit1 = 2000
+ad1 = 1000
+# shaheer.sales
+
+sales2 = 6000
+profit2 = 2000
+ad2 = 1000 
+# seerat.sales
+
+sales3 = 6000
+profit3 = 20
+ad3 = 1000
+
+# 2. Object Oriented Programming: Make things easier, easy to understand, Maintainable
+
+"""RilwayForm   ---> Class [blueprint]
+harry --> harry ki info wala form --> Object [entity]
+tom --> tom ki info wala form --> Object [entity]
+shubham -- shubham ki info wala form --> Object [entity]
+shubham.changeName("Shubhi")"""
